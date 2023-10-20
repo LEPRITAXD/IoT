d4ac1cbd9696ed20d28a6c108e979feadb7fbcca
frank <fefernandez.0972@unicesmag.edu.co>
2023-10-20 11:04:47 -0500

    Ejercicio_semaforos_faltantes

---
+++ Arduino/reto2/reto2.ino
@@ -0,0 +1,27 @@
+//FRANK ESTEBAN FERNANDEZ
+int led_verd=10;
+int led_amar=9;
+int led_rojo=8;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led_verd,OUTPUT);//No Pin y Mode (OUTPUT/INPUT)
+  pinMode(led_amar,OUTPUT);
+  pinMode(led_rojo,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  digitalWrite(led_rojo,HIGH);
+  delay(500); //1000=1 SEG
+  digitalWrite(led_rojo,LOW);
+  delay(1000); //1000=1 SEG
+  digitalWrite(led_amar,HIGH);
+  delay(500); //1000=1 SEG
+  digitalWrite(led_amar,LOW);
+  delay(1000); //1000=1 SEG
+  digitalWrite(led_verd,HIGH);
+  delay(500); //1000=1 SEG
+  digitalWrite(led_verd,LOW);
+  delay(1000); //1000=1 SEG
+}
---
+++ Arduino/reto3/reto3.ino
@@ -0,0 +1,28 @@
+// FRANK ESTEBAN FERNANDEZ
+int led_verd=10;
+int led_amar=9;
+int led_rojo=8;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led_verd,OUTPUT);//No Pin y Mode (OUTPUT/INPUT)
+  pinMode(led_amar,OUTPUT);
+  pinMode(led_rojo,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i=0; i<5 ;i++){ 
+    digitalWrite(led_rojo,HIGH);
+    digitalWrite(led_verd,HIGH);
+    delay(100); //1000=1 SEG
+    digitalWrite(led_rojo,LOW);
+    digitalWrite(led_verd,LOW);
+    delay(1000); //1000=1 SEG
+  }
+  for (int j=0; j<5 ;j++){
+    digitalWrite(led_amar,HIGH);
+    delay(100); //1000=1 SEG
+    digitalWrite(led_amar,LOW);
+    delay(1000); //1000=1 SEG
+  }
+}
